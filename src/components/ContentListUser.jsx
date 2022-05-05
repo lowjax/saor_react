61342d5ddfa01ebc46080b8a348c2ba93da01496
Jack Cooke <jack.wheet.cooke@gmail.com>
2022-05-05 14:49:54 +1000

    content loop

--- src/components/ContentListUser.jsx
+++ src/components/ContentListUser.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PhysioContent from "../img/PhysioContent.jpg"
 import NavbarUser from "./NavbarUser";
 
+import { Content } from "../contentdata";
 
 export default function ContentListUser() {
   return (
@@ -12,15 +13,12 @@ export default function ContentListUser() {
       <NavbarUser />
       <div id="contentContainer">
         <div className="card-group">
-          <div class="card">
+          {Content.map(content => (
+            <div class="card">
             <div className="card-body" id="contentCard">
               <img id="physioImage" src={PhysioContent} height={40}/>
-              <h4 className="card-title">Routine 1</h4>
-              <p className="card-text">
-                Nullam id dolor id nibh ultricies vehicula ut id elit. Cras
-                justo odio, dapibus ac facilisis in, egestas eget quam. Donec id
-                elit non mi porta gravida at eget metus.
-              </p>
+              <h4 className="card-title">{content.id}</h4>
+              <p className="card-text">{content.Name}</p>
               <button
                 className="btn btn-primary"
                 id="favoritesButton"
@@ -30,7 +28,8 @@ export default function ContentListUser() {
               </button>
             </div>
           </div>
-          <div class="card">
+          ))}
+          {/* <div class="card">
             <div className="card-body" id="contentCard">
               <img id="physioImage" src={PhysioContent} height={40} />
               <h4 className="card-title">Routine 2</h4>
@@ -47,7 +46,7 @@ export default function ContentListUser() {
                 ADD TO FAVORITES
               </button>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
